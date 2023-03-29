693b9454abd2d1d67a85f3d38420dbba4f215932
Nicolás López <nicolaslopezj@me.com>
2023-03-29 15:42:24 -0300

    export store

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import fireEvent from './fireEvent'
+import {addListener, getListeners, removeListener} from './store'
 import useOnEvent from './useOnEvent'
 import waitAndFire from './waitAndFire'
 
-export {fireEvent, useOnEvent, waitAndFire}
+export {fireEvent, useOnEvent, waitAndFire, addListener, removeListener, getListeners}
